70586dd19192e733c3f4b71bcb5b0da23555a01e
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-04-11 21:23:07 +0300

    task10

---
+++ task_10.py
@@ -0,0 +1,15 @@
+count_money = int(input())
+i = 1
+count_false = 0
+count_true = 0
+while count_money >= i:
+    side = int(input())
+    if (side == 0):
+        count_false += 1
+    if( side == 1):
+        count_true += 1
+    i += 1 
+if (count_true > count_false):
+    print(count_false)
+else:
+    print(count_true)
