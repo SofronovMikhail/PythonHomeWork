0c160272fa83a2b44fc8584f3fee6016540f09d0
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-04-15 23:15:45 +0300

    task_18

---
+++ task_18.py
@@ -0,0 +1,16 @@
+array = list(map(int, input("Введите элементы массива: ").split()))
+user_num = int(input("Введите искомое число: "))
+i = 0
+index = 0
+array_end = list()
+while (i < len(array)):
+    array_end.append(array[i] - user_num)
+    if (array_end[i] < 0):
+        array_end[i] *= -1
+    i += 1
+min = min(array_end)
+for i in range(len(array_end)):
+     if (min == array_end[i]):
+        index = i
+        break
+print(f"Самое ближнее минимальное число: {array[index]}")
