105ff333a929cfa0e6c38ac5ce12be4f3772b74b
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-04-18 14:03:57 +0300

    task22

---
+++ task_22.py
@@ -0,0 +1,7 @@
+array1 = list(map(int, input("Введите первый набор целых чисел через пробел: ").split()))
+array2 = list(map(int, input("Введите первый набор целых чисел через пробел: ").split()))
+sum_array = set()
+array = array1 + array2
+for i in range(len(array)):
+    sum_array.add(array[i])
+print(*sum_array)
